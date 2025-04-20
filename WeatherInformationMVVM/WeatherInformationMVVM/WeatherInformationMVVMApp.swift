6aa4a4a8a69cfd8bd461080c2a3232deb42a859c
Koki2022 <kokikoki3219@icloud.com>
2025-04-21 00:56:34 +0900

    天気アプリMVVMバージョン

---
+++ WeatherInformationMVVM/WeatherInformationMVVM/WeatherInformationMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherInformationMVVMApp.swift
+//  WeatherInformationMVVM
+//
+//  Created by 高橋昴希 on 2025/04/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherInformationMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
